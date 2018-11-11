2d1cc54e17ab18216446711e3894756742f5ed90
adnaan.ahmed <adnaan.ahmed@flipkart.com>
2018-11-11 19:14:20 +0530

    added lingui dependencies and extracted the main code to the src directory

--- App.js
+++ App.js
@@ -1,21 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import MainComponent from './src/MainComponent';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello World!</Text>
-      </View>
+      <MainComponent/>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
